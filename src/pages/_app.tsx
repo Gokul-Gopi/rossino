fae9835838573fc990c3300a05044dd6ef7c3ae4
Gokul-Gopi <gokulg1412@gmail.com>
2025-10-21 15:07:52 +0000

    feat: cursor-pointer on button, toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
+import toasterOptions from "@/utils/toaster";
 import type { AppProps } from "next/app";
+import { Toaster } from "sonner";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster {...toasterOptions} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/Button";
+import { toast } from "sonner";
+
 const Home = () => {
-  return <div>Rossino</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-dvh">
+      <Button onClick={() => toast.error("My first toast!")}>Click</Button>
+    </div>
+  );
 };
 
 export default Home;
