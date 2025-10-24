491322693b91e8d599425cf17bc53beb8fe51ffc
Gokul-Gopi <gokulg1412@gmail.com>
2025-10-24 16:26:21 +0000

    style: toast  position: "bottom-center",

--- src/utils/toaster.ts
+++ src/utils/toaster.ts
@@ -2,6 +2,21 @@ import type { ToasterProps } from "sonner";
 
 const toasterOptions: ToasterProps = {
   closeButton: true,
+  position: "bottom-center",
+  className: "text-red-900!",
+  duration: 400000,
+  expand: false,
+  visibleToasts: 3,
+  toastOptions: {
+    classNames: {
+      content: "font-sans! text-sm!",
+      success:
+        "bg-sidebar! text-foreground! border-green-500! *:data-icon:text-green-500!",
+      error:
+        "bg-sidebar! text-foreground! border-red-500! *:data-icon:text-red-500!",
+      closeButton: "bg-sidebar! text-foreground! border! border-gray-400!",
+    },
+  },
 };
 
 export default toasterOptions;
