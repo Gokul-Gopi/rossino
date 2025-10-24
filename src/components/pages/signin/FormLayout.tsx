c590ece79b9203cdb0702e340eb3a3489cc49c22
Gokul-Gopi <gokulg1412@gmail.com>
2025-10-24 05:36:06 +0000

    feat: animation added to smooth out button position shift

--- src/components/pages/signin/FormLayout.tsx
+++ src/components/pages/signin/FormLayout.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import tomato from "../../../../public/assets/tomato.avif";
 import googleIcon from "../../../../public/assets/google-icon.svg";
 import { Button } from "@/components/ui/Button";
+import { LayoutGroup, motion } from "motion/react";
 
 const FormLayout = () => {
   const [isLoginForm, setIsLoginForm] = useState(true);
@@ -12,30 +13,43 @@ const FormLayout = () => {
   return (
     <div className="flex items-center w-full max-w-4xl px-4">
       <div className="bg-card w-full items-center min-h-[34rem] flex relative gap-4 shadow p-4 border rounded-lg">
-        <div className="flex-1">
-          <Button
-            size="lg"
-            variant="outline"
-            className="w-full hover:bg-background hover:text-black hover:shadow"
-          >
-            <Image src={googleIcon} alt="google icon" width={18} height={18} />
-            Continue with Google
-          </Button>
+        <LayoutGroup>
+          <div className="flex-1">
+            <motion.div layout transition={{ duration: 0.3 }}>
+              <Button
+                size="lg"
+                variant="outline"
+                className="w-full hover:bg-background hover:text-black hover:shadow"
+              >
+                <Image
+                  src={googleIcon}
+                  alt="google icon"
+                  width={18}
+                  height={18}
+                />
+                Continue with Google
+              </Button>
+            </motion.div>
 
-          <div className="flex items-center gap-2 my-4">
-            <hr className="w-full" />
-            <span className="font-medium text-sm text-muted-foreground">
-              OR
-            </span>
-            <hr className="w-full" />
+            <motion.div
+              transition={{ duration: 0.3 }}
+              layout
+              className="flex items-center gap-2 my-4"
+            >
+              <hr className="w-full" />
+              <span className="font-medium text-sm text-muted-foreground">
+                OR
+              </span>
+              <hr className="w-full" />
+            </motion.div>
+
+            {isLoginForm ? (
+              <LoginForm switchForm={() => setIsLoginForm(!isLoginForm)} />
+            ) : (
+              <SignupForm switchForm={() => setIsLoginForm(!isLoginForm)} />
+            )}
           </div>
-
-          {isLoginForm ? (
-            <LoginForm switchForm={() => setIsLoginForm(!isLoginForm)} />
-          ) : (
-            <SignupForm switchForm={() => setIsLoginForm(!isLoginForm)} />
-          )}
-        </div>
+        </LayoutGroup>
 
         <div className="flex-1 relative self-stretch max-lg:hidden">
           <Image
