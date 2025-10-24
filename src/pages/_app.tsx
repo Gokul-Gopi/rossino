4f9360f2d354e6b5dfbe4f8b7003568a4f3396bf
Gokul-Gopi <gokulg1412@gmail.com>
2025-10-24 08:47:15 +0000

    feat: supabase and tanstack-query client setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,35 @@ import ThemeProvider from "@/components/ui/ThemeProvider";
 import "@/styles/globals.css";
 import toasterOptions from "@/utils/toaster";
 import type { AppProps } from "next/app";
+import { useState } from "react";
 import { Toaster } from "sonner";
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
   return (
-    <ThemeProvider attribute="class" enableSystem={false}>
-      <Component {...pageProps} />
-      <Toaster {...toasterOptions} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <ThemeProvider attribute="class" enableSystem={false}>
+          <Component {...pageProps} />
+          <Toaster {...toasterOptions} />
+        </ThemeProvider>
+      </HydrationBoundary>
+    </QueryClientProvider>
   );
 }
