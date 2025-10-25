c62681dabca8a5db417e6cf1ceb177db831f4384
Gokul-Gopi <gokulg1412@gmail.com>
2025-10-25 08:16:58 +0000

    style: toaster on dark mode

--- src/utils/toaster.tsx
+++ src/utils/toaster.tsx
@@ -15,12 +15,14 @@ const toasterOptions: ToasterProps = {
     classNames: {
       toast: "gap-2.5! w-[18.75rem]!",
       content: "font-sans! text-sm! line-clamp-5!",
+      default:
+        "bg-gradient-to-r! from-seondary! text-gray-800! dark:bg-gradient-to-br! dark:from-secondary! dark:from-50%! dark:text-foreground!",
       success:
-        "bg-gradient-to-r! to-green-400/40! from-sidebar! text-gray-800! border-green-400! *:data-icon:text-green-500!",
+        "to-green-400/40! dark:to-green-400! border-green-400! *:data-icon:text-green-500!",
       error:
-        "bg-gradient-to-r! to-red-400/40! from-sidebar! text-gray-800! border-red-400! *:data-icon:text-red-500!",
+        "to-red-400/40! dark:to-red-400! border-red-400! *:data-icon:text-red-500!",
       closeButton:
-        "bg-secondary! text-foreground! border! border-gray-400! -top-0.5! -right-4! left-[unset]! hover:bg-gray-200!",
+        "bg-secondary! text-foreground! border! border-gray-400! -top-0.5! -right-4! left-[unset]! hover:bg-gray-200! dark:hover:bg-black!",
     },
   },
 };
