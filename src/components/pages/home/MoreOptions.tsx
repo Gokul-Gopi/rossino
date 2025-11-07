b521e565a8224bdebce5b49ea630a253d0c80dc9
Gokul-Gopi <gokulg1412@gmail.com>
2025-11-07 15:23:09 +0000

    feat: reset timer for current session

--- src/components/pages/home/MoreOptions.tsx
+++ src/components/pages/home/MoreOptions.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/Button";
+import ConfirmDialog from "@/components/ui/ConfirmDialog";
 import { Label } from "@/components/ui/Label";
 import {
   Popover,
@@ -6,48 +7,78 @@ import {
   PopoverTrigger,
 } from "@/components/ui/Popover";
 import { Switch } from "@/components/ui/Switch";
-import { useWidgetsStore, useTaskStore } from "@/store";
+import { useWidgetsStore, useTaskStore, useSessionStore } from "@/store";
 import { ChevronDown } from "lucide-react";
+import { useState } from "react";
 
 const MoreOptions = () => {
-  const { showTasks, toggleTasksVisibility } = useTaskStore();
+  const [confirmReset, setConfirmReset] = useState(false);
+
+  const { setSession } = useSessionStore();
   const { showWidgets, toggleWidgets } = useWidgetsStore();
+  const { showTasks, toggleTasksVisibility } = useTaskStore();
+
+  const onResetTimer = () => {
+    setSession({
+      startedAt: null,
+      endedAt: null,
+      lastPausedAt: null,
+      totalPausedDuration: 0,
+      elapsedTime: 0,
+      status: "IDLE",
+    });
+    setConfirmReset(false);
+  };
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button className="text-primary group absolute right-0 -bottom-12 bg-transparent px-0! font-medium hover:bg-transparent">
-          More options{" "}
-          <ChevronDown className="transition-transform group-data-[state=open]:rotate-180" />
-        </Button>
-      </PopoverTrigger>
+    <>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button className="text-primary group absolute right-0 -bottom-12 bg-transparent px-0! font-medium hover:bg-transparent active:scale-100">
+            More options{" "}
+            <ChevronDown className="transition-transform group-data-[state=open]:rotate-180" />
+          </Button>
+        </PopoverTrigger>
 
-      <PopoverContent
-        align="end"
-        className="bg-card flex w-[10rem] flex-col gap-3"
-      >
-        <div className="flex items-center justify-between">
-          <Label htmlFor="tasks">Tasks</Label>
-          <Switch
-            id="tasks"
-            checked={showTasks}
-            onCheckedChange={toggleTasksVisibility}
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <Label htmlFor="widgets">Widgets</Label>
-          <Switch
-            id="widgets"
-            checked={showWidgets}
-            onCheckedChange={toggleWidgets}
-          />
-        </div>
+        <PopoverContent
+          align="end"
+          className="bg-card flex w-[10rem] flex-col gap-3"
+        >
+          <div className="flex items-center justify-between">
+            <Label htmlFor="tasks">Tasks</Label>
+            <Switch
+              id="tasks"
+              checked={showTasks}
+              onCheckedChange={toggleTasksVisibility}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <Label htmlFor="widgets">Widgets</Label>
+            <Switch
+              id="widgets"
+              checked={showWidgets}
+              onCheckedChange={toggleWidgets}
+            />
+          </div>
 
-        <Button size="sm" variant="outline" className="text-sm">
-          Reset Timer
-        </Button>
-      </PopoverContent>
-    </Popover>
+          <Button
+            onClick={() => setConfirmReset(true)}
+            size="sm"
+            variant="outline"
+            className="text-sm"
+          >
+            Reset Timer
+          </Button>
+        </PopoverContent>
+      </Popover>
+
+      <ConfirmDialog
+        open={confirmReset}
+        onOpenChange={() => setConfirmReset((pre) => !pre)}
+        title="This will reset the current session's timer. Continue?"
+        onConfirm={onResetTimer}
+      />
+    </>
   );
 };
 export default MoreOptions;
