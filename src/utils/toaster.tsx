ce5a147c60f10a447db1155947a27bbf8dfcfca9
Gokul-Gopi <gokulg1412@gmail.com>
2025-11-19 16:46:13 +0000

    style: better toast style

--- src/utils/toaster.tsx
+++ src/utils/toaster.tsx
@@ -12,18 +12,18 @@ const toasterOptions: ToasterProps = {
     error: <CircleAlert />,
     info: <Info />,
   },
+  className: "flex justify-center",
   toastOptions: {
     classNames: {
-      toast: "gap-2.5!",
+      toast: "gap-2.5! max-w-[17rem]",
       description: "text-xs dark:text-gray-400!",
-      content: "font-sans! text-sm! line-clamp-5!",
+      content: "font-sans! text-sm! line-clamp-5! ",
       default:
-        "bg-gradient-to-r! from-seondary! text-gray-800! dark:bg-gradient-to-br! dark:from-secondary! dark:from-50%! dark:text-foreground!",
+        "bg-gradient-to-r! from-card! text-gray-800! border-input! dark:bg-gradient-to-br! dark:from-secondary! dark:from-50%! dark:text-foreground!",
       success:
-        "to-green-400/40! dark:to-green-400! border-green-400! *:data-icon:text-green-500!",
-      info: "to-blue-400/40! dark:to-blue-400! border-blue-400! *:data-icon:text-blue-500!",
-      error:
-        "to-red-400/40! dark:to-red-400! border-red-400! *:data-icon:text-red-500!",
+        "to-green-400/40! dark:to-green-400! *:data-icon:text-green-500!",
+      info: "to-blue-400/40! dark:to-blue-400! *:data-icon:text-blue-500!",
+      error: "to-red-400/40! dark:to-red-400! *:data-icon:text-red-500!",
       closeButton:
         "bg-secondary! text-foreground! border! border-gray-400! -top-0.5! -right-4! left-[unset]! hover:bg-gray-200! dark:hover:bg-black!",
     },
